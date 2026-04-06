966f6709b134f8148e6994fae0e9c308a107da6a
elenaferr0 <elena.ferro@gmail.com>
2026-04-06 14:17:38 +0200

    feat: github actions deploy

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+var _a, _b;
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+var repoName = (_b = (_a = process.env.GITHUB_REPOSITORY) === null || _a === void 0 ? void 0 : _a.split('/')[1]) !== null && _b !== void 0 ? _b : 'kosmospedia';
+var base = process.env.GITHUB_ACTIONS ? "/".concat(repoName, "/") : '/';
+export default defineConfig({
+    base: base,
+    plugins: [react()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: ['./src/test/setup.ts'],
+    },
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? 'kosmospedia';
+const base = process.env.GITHUB_ACTIONS ? `/${repoName}/` : '/';
+
 export default defineConfig({
+  base,
   plugins: [react()],
   test: {
     globals: true,
